3eb766389669ec90723d99e4b223771a82b4601c
root <root@TrueNAS.local>
2026-02-20 21:40:44 +0000

    Upgrade plugin to FreeBSD 13.5, MariaDB 10.11, PHP 8.3

--- README.md
+++ README.md
@@ -1,12 +1,87 @@
 # iocage-plugin-piwigo
 
-This is plugin to create Piwigo, an open source photo gallery software for the web. Designed for organisations, teams and individuals.
+This is iocage plugin to create Piwigo 14.x, an open source photo gallery software for the web. Designed for organisations, teams and individuals.
+More details at http://piwigo.org
 
-I have manage to test this couple of times on my TrueNAS 12.2, all seems to work well.
+*I have tested this plugin couple of times on my TrueNAS 13 U5 as a **13.1-RELEASE**, all seems to work well.*
 
-Sometimes during the first installation screen, when entering data regarding the database, db user and db password, Host: need to be replaced from localhost to 127.0.0.1
-All data required for this first install-screen are saved in TrueNAS/Plugins/Piwigo/Post Install Notes.
+![Piwigo Installation Successful](https://i.imgur.com/p53XnmOl.png)
 
-Second tip is setting your own date and time location in PHP.INI, for standart setting I have used Europe/London ;)
+Tip 1. Please remember to read info in **TrueNAS / Plugins / Piwigo / POST INSTALL NOTES** - to access info with DB user and DB password.
+Piwigo first installation page will set Host: as localhost, usually this needs to be changed to 127.0.0.1
+>   Host: 127.0.0.1
 
+Tip 2. Please set your own date/time location in PHP.INI, as for this installation I have chosen Europe/London ;)
+
+To install Piwigo plugin manually from the internet:
+
+> iocage fetch -P piwigo -g https://github.com/AfroUSApl/iocage-plugin-piwigo ip4_addr="interface|IPaddress"
+
+where *interface* is the name of the active network interface and *IP address* is the desired IP address for the plugin. For example, ip4_addr="igb0|192.168.0.91"
+
+
+## Some of the post installation settings I have tuned for Piwigo:
+<h6> PHP
+
+```
+    date.timezone = "Europe/London"
+    max_execution_time = 300
+    max_input_time = 300
+    post_max_size = 100M
+    upload_max_filesize = 100M
+    memory_limit = 512M
+```
+Enable this extension for mysqli in PHP.ini
+
+```
+    extension=mysqli
+```
+
+<h6>Nginx
+
+```
+    proxy_connect_timeout 600s
+    proxy_send_timeout 600s
+    proxy_read_timeout 600s
+    fastcgi_send_timeout 600s
+    fastcgi_read_timeout 600s
+
+    pm.max_children = 35
+    pm.start_servers = 15
+    pm.min_spare_servers = 15
+    pm.max_spare_servers = 20
+
+    request_terminate_timeout = 300
+```
+
+<h2> <h2>Gallery View
+
+![Piwigo Gallery View - Theme Modus](https://i.imgur.com/OfVd8fUl.jpg)
+
+![Piwigo Dashboard View](https://i.imgur.com/hPlxgwbl.jpg)
+
+## Update... 9/1/2024
+
+I successfully updated my iocage Release 12.2 to 13.1, this is my procedure:
+<h6>outside the iocage
+
+```
+iocage upgrade -r 13.1 piwigo
+
+Note: This will take some time
+- Type "y" for yes when inquiries occur
+- Type "q" for "quit" when the validation list appears, you may need to type it several times depending on the number of affected items
+- Type "y" for yes for any additional inquiries
+```
+
+<h6>inside the iocage
+
+```
+pkg update && pkg upgrade -y
+
+pkg install php83 php83-session php83-mysqli nginx mariadb105-server ImageMagick7-nox11 git php83-exif php83-filter php83-gd php83-mbstring php83-zip php83-zlib php83-pecl-json_post finfo php83-fileinfo
+```
+Than restart iocage and log in to your gallery. I had an option to upgrade Piwigo to 14.1.0 ;)
+
+![Piwigo Maintenance View](https://i.imgur.com/CaKbBkHl.png)
 
--- post_install.sh
+++ post_install.sh
@@ -1,101 +1,173 @@
 #!/bin/sh
 
+export LC_ALL=C
+
+##############################################################################
+# Enable services
+##############################################################################
+
 # Enable the service
-sysrc -f /etc/rc.conf nginx_enable="YES"
-sysrc -f /etc/rc.conf mysql_enable="YES"
-sysrc -f /etc/rc.conf php_fpm_enable="YES"
+sysrc nginx_enable=YES
+sysrc php_fpm_enable=YES
+sysrc mysql_enable=YES
+
+##############################################################################
+# Start MariaDB first
+##############################################################################
+
 # Start the service
-service nginx start 2>/dev/null
-service php-fpm start 2>/dev/null
-service mysql-server start 2>/dev/null
+service mysql-server start
 
-# Create user 'piwigo'
-pw user add -n piwigo -s /sbin/nologin -c "Piwigo"
-# Copy a base MySQL configuration to use
-cp /usr/local/etc/mysql/my-small.cnf /usr/local/etc/mysql/my.cnf
-# Configure the default PHP settings
-cp /usr/local/etc/php.ini-production /usr/local/etc/php.ini
+##############################################################################
+# Secure MariaDB (10.11 compatible)
+##############################################################################
 
-# Modify settings in php.ini for Piwigo best performance
-sed -i '' 's/.*max_execution_time = .*/max_execution_time = 300/' /usr/local/etc/php.ini
-sed -i '' 's/.*max_input_time = .*/max_input_time = 300/' /usr/local/etc/php.ini
-sed -i '' 's/.*post_max_size = .*/post_max_size = 100M/' /usr/local/etc/php.ini
-sed -i '' 's/.*upload_max_filesize = .*/upload_max_filesize=100M/' /usr/local/etc/php.ini
-# recommended value of 512MB for php memory limit (avoid warning)
-sed -i '' 's/.*memory_limit = .*/memory_limit = 512M/' /usr/local/etc/php.ini
-sed -i '' 's/.*;date.timezone =.*/date.timezone = "Europe\/London"/' /usr/local/etc/php.ini
+# Remove anonymous users and test database
+mysql -u root <<EOF
+DELETE FROM mysql.user WHERE User='';
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+FLUSH PRIVILEGES;
+EOF
 
-# Create a configuration directory to make managing individual server blocks easier
-mkdir /usr/local/etc/nginx/conf.d
-mkdir /usr/local/etc/php-fpm.d
-# Editing WWW config file - www.conf
-grep -qxF 'request_terminate_timeout = 300' /usr/local/etc/php-fpm.d/www.conf || echo 'request_terminate_timeout = 300' >> /usr/local/etc/php-fpm.d/www.conf
-# Editing PHP-FPM config file - php-fpm.conf
-grep -qxF 'include=/usr/local/etc/php-fpm.d/*.conf' /usr/local/etc/php-fpm.conf || echo 'include=/usr/local/etc/php-fpm.d/*.conf' >> /usr/local/etc/php-fpm.conf
-
+##############################################################################
 # Create user and database for Piwigo with unique password
+##############################################################################
+
 USER="piwigouser"
 DB="piwigodb"
+
 # Save the config values
 echo "$DB" > /root/dbname
 echo "$USER" > /root/dbuser
-export LC_ALL=C
-cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1 > /root/dbpassword
-PASS=`cat /root/dbpassword`
+
+# Generate secure random password
+PASS=$(tr -dc 'a-zA-Z0-9' </dev/urandom | fold -w 20 | head -n 1)
+echo "$PASS" > /root/dbpassword
+
 echo "Database User: $USER"
 echo "Database Password: $PASS"
-if [ -e "/root/.mysql_secret" ] ; then
-   # Mysql > 57 sets a default PW on root
-   TMPPW=$(cat /root/.mysql_secret | grep -v "^#")
-   echo "SQL Temp Password: $TMPPW"
-# Configure mysql db
-echo "ROOT Temp Password: $TMPPW"
-mysql -u root -p"${TMPPW}" --connect-expired-password <<-EOF
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${PASS}';
-CREATE DATABASE ${DB} CHARACTER SET utf8;
+
+# Create database and user (MariaDB 10.11 compatible)
+mysql -u root <<EOF
+CREATE DATABASE ${DB} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE USER '${USER}'@'localhost' IDENTIFIED BY '${PASS}';
 GRANT ALL PRIVILEGES ON ${DB}.* TO '${USER}'@'localhost';
 FLUSH PRIVILEGES;
 EOF
-# Make the default log directory
-mkdir /var/log/zm
-chown www:www /var/log/zm
 
-else
+##############################################################################
+# Configure PHP
+##############################################################################
 
-# Mysql <= 56 does not
-# Configure mysql
-mysql -u root <<-EOF
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$TMPPW';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-DELETE FROM mysql.user WHERE User='';
-DELETE FROM mysql.db WHERE Db='test' OR Db='test_%';
+# Copy a base PHP configuration
+cp /usr/local/etc/php.ini-production /usr/local/etc/php.ini
 
-CREATE DATABASE ${DB} CHARACTER SET utf8;
-CREATE USER '${USER}'@'localhost' IDENTIFIED BY '${PASS}';
-GRANT ALL PRIVILEGES ON *.* TO '${USER}'@'localhost' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON ${DB}.* TO '${USER}'@'localhost';
-FLUSH PRIVILEGES;
+# Modify settings in php.ini for Piwigo best performance
+# recommended value of 512MB for php memory limit (avoid warning)
+sed -i '' 's/^memory_limit = .*/memory_limit = 512M/' /usr/local/etc/php.ini
+sed -i '' 's/^upload_max_filesize = .*/upload_max_filesize = 512M/' /usr/local/etc/php.ini
+sed -i '' 's/^post_max_size = .*/post_max_size = 512M/' /usr/local/etc/php.ini
+sed -i '' 's/^max_execution_time = .*/max_execution_time = 300/' /usr/local/etc/php.ini
+sed -i '' 's/^;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/' /usr/local/etc/php.ini
+
+##############################################################################
+# Configure PHP-FPM
+##############################################################################
+
+# Editing WWW config file - www.conf
+sed -i '' 's/^user = .*/user = www/' /usr/local/etc/php-fpm.d/www.conf
+sed -i '' 's/^group = .*/group = www/' /usr/local/etc/php-fpm.d/www.conf
+sed -i '' 's/^listen = .*/listen = 127.0.0.1:9000/' /usr/local/etc/php-fpm.d/www.conf
+
+# Editing PHP-FPM config file - php-fpm.conf
+sed -i '' 's/^;daemonize = yes/daemonize = yes/' /usr/local/etc/php-fpm.conf
+
+##############################################################################
+# Configure Nginx
+##############################################################################
+
+# Create a configuration directory to make managing individual server blocks easier
+mkdir -p /usr/local/etc/nginx/conf.d
+
+cat > /usr/local/etc/nginx/nginx.conf <<EOF
+user  www;
+worker_processes  auto;
+
+events {
+    worker_connections  1024;
+}
+
+http {
+    include       mime.types;
+    default_type  application/octet-stream;
+
+    sendfile        on;
+    keepalive_timeout  65;
+
+    include /usr/local/etc/nginx/conf.d/*.conf;
+}
 EOF
-fi
 
-# Download Piwigo version 11.x from GitHub
-cd /usr/local/www
-git clone -b 11.x https://github.com/Piwigo/Piwigo.git
-# Create Piwigo data directory with permission
-mkdir /usr/local/www/Piwigo/_data
-chmod -R 777 /usr/local/www/Piwigo/_data
-# Change the ownership of the whole Piwigo directory
-chown -R piwigo:www /usr/local/www/Piwigo
+cat > /usr/local/etc/nginx/conf.d/piwigo.conf <<EOF
+server {
+    listen 80;
+    server_name _;
 
-#restart the services to make sure we have pick up the new permission
-service php-fpm restart 2>/dev/null
-#nginx restarts to fast while php is not fully started yet
-sleep 5
-service nginx restart 2>/dev/null
+    root /usr/local/www/nginx/piwigo;
+    index index.php index.html;
 
-# Add plugin detals to info file available in TrueNAS Plugin Additional Info
+    location / {
+        try_files \$uri \$uri/ /index.php?\$query_string;
+    }
+
+    location ~ \.php$ {
+        include fastcgi_params;
+        fastcgi_pass 127.0.0.1:9000;
+        fastcgi_index index.php;
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+    }
+
+    location ~ /\.ht {
+        deny all;
+    }
+}
+EOF
+
+##############################################################################
+# Download and install latest Piwigo
+##############################################################################
+
+mkdir -p /usr/local/www/nginx
+cd /usr/local/www/nginx
+
+fetch https://piwigo.org/download/dlcounter.php?code=latest -o piwigo.zip
+unzip piwigo.zip
+rm piwigo.zip
+
+chown -R www:www /usr/local/www/nginx
+
+##############################################################################
+# Start services
+##############################################################################
+
+service php-fpm start
+service nginx start
+
+##############################################################################
+# Add plugin details to info file available in TrueNAS Plugin Additional Info
+##############################################################################
+
 echo "Host: 127.0.0.1" > /root/PLUGIN_INFO
 echo "Database User: $USER" >> /root/PLUGIN_INFO
 echo "Database Password: $PASS" >> /root/PLUGIN_INFO
 echo "Database Name: $DB" >> /root/PLUGIN_INFO
+
+echo "------------------------------------------------------------"
+echo " Piwigo installation complete"
+echo "------------------------------------------------------------"
+echo "Host: 127.0.0.1"
+echo "Database Name: $DB"
+echo "Database User: $USER"
+echo "Database Password: $PASS"
+echo "------------------------------------------------------------"
